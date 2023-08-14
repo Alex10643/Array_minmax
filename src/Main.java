1288f9a03a889ebd504154e262f2b4ebf8404795
Alex <alex.shalimovlol@gmail.com>
2023-08-14 19:11:05 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {5, 22, 49, 13, 31};
+        int min = array[0];
+        int max = array[0];
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+
+        System.out.println(min);
+        System.out.println(max);
+    }
+}
