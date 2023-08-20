affec9ea9eee896daed0d0c0555fdccf181f1d4a
Alex <alex.shalimovlol@gmail.com>
2023-08-20 15:30:14 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,35 @@
 
-
 public class Main {
-    public static void main(String[] args) {
-        //TODO add method min/max and add code there
-        //TODO add method bubbleSort to sort this array
-        int[] array = {5, 22, 49, 13, 31};
-        int min = array[0];
-        int max = array[0];
 
+    public static void main(String[] args) {
+        minMax();
+    }
+
+    public static void minMax() {
+
+        int[] array = {5, 22, 49, 13, 31};
+        min(array);
+        max(array);
+    }
+
+    public static void min(int[] array) {
+
+        int min = array[0];
         for (int i = 0; i < array.length; i++) {
             if (array[i] < min) {
                 min = array[i];
             }
         }
+        System.out.println(min);
+    }
 
+    public static void max(int[] array) {
+        int max = array[0];
         for (int i = 0; i < array.length; i++) {
             if (array[i] > max) {
                 max = array[i];
             }
         }
-
-        System.out.println(min);
         System.out.println(max);
     }
 }
